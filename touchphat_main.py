ff386f2a7b9b5eb6b1f9f39ba71aff3b6e5ba9b9
nc30 <nc30@users.noreply.github.com>
2018-11-07 13:31:11 +0900

    Update touchphat_main.py

--- touchphat_main.py
+++ touchphat_main.py
@@ -1,7 +1,28 @@
 #! /usr/bin/env python3
+
+```
+    touchphat_mqtt_controllerを始動させるサンプル
+    別途環境変数の指定が必要です。
+
+    
+    必要な環境変数は以下のとおりです。
+    
+    MQTT_TARGET_NAME 表示制御を行うUnicornHatHDで指定した名前
+    MQTT_NAME MQTT上で使用する名前
+    MQTT_HOST MQTTホスト名
+    MQTT_USER MQTTユーザー名
+    MQTT_PASSWORD MQTTパスワード
+    MQTT_PORT MQTTポート
+ ```
+
+# ロギング設定
+# 今回は標準出力でINFO以上のログを出力する
+
+# ロガー作成
 from logging import getLogger
 logger = getLogger('touchphat_mqtt_controller')
 
+# ハンドラの指定（出力設定）
 from logging import Formatter, StreamHandler, INFO
 import sys
 import os
@@ -15,14 +36,14 @@ handler.setFormatter(
 )
 logger.addHandler(handler)
 
+
+# ここから始動処理
+
 from touchphat_mqtt_controller import main
-import signal
 
 if __name__ == '__main__':
     try:
         main()
-        signal.pause()
-
     except KeyboardInterrupt:
         pass
 
